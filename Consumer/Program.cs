2b1b6f820ffbc529bb3f51f989c9b2882422e45d
maciekjaw <maciekjaworski@wp.pl>
2019-11-27 11:52:03 +0100

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProducerNamespace;
+
+namespace ConsumerNamespa
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumeMessages cM = new ConsumeMessages();
+            cM.Consume();
+        }
+    }
+
+    public class ConsumeMessages
+    {
+        public void Consume()
+        {
+            ProduceMessages pM = new ProduceMessages();
+
+            string topic = "cars";
+
+            Uri uri = new Uri("http://localhost:9092");
+            var options = new KafkaOptions(uri);
+            var router = new BrokerRouter(options);
+            var newrouter = new BrokerRouter(options);
+            ConsumerOptions cO = new ConsumerOptions(topic, router);
+            var consumer = new Consumer(cO);
+
+            foreach (var message in consumer.Consume())
+            {
+                List<string> setOfMessages = new List<string>();
+                setOfMessages.Add(Encoding.UTF8.GetString(message.Value));
+                Console.WriteLine(Encoding.UTF8.GetString(message.Value));
+                Assert.AreEqual(setOfMessages[0], pM.BrandName);
+                Assert.AreEqual(setOfMessages[1], pM.Model);
+                Assert.AreEqual(setOfMessages[2], pM.NumberOfDoors);
+                Assert.AreEqual(setOfMessages[3], pM.IsSport);
+            }
+
+        }
+
+    }
+}
---
+++ Kafka2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using KafkaNet.Model;
+using KafkaNet;
+using System.Collections.Generic;
+using KafkaNet.Protocol;
+
+namespace ProducerNamespace
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ProduceMessages pM = new ProduceMessages();
+            pM.Producer();
+        }
+    }
+
+    public class ProduceMessages
+    {
+        Message brandName = new Message("Mercedes");
+        Message model = new Message("GLS");
+        Message numberOfDoors = new Message("5");
+        Message isSport = new Message("yes");
+
+        public Message BrandName { get { return new Message("Mercedes"); } }
+        public Message Model { get { return new Message("GLS"); } }
+        public Message NumberOfDoors { get { return new Message("5"); } }
+        public Message IsSport { get { return new Message("yes"); } }
+
+        public void Producer()
+        {
+            string topic = "cars";
+
+            List<Message> messages = new List<Message>() { brandName,model,numberOfDoors,isSport };
+
+            Uri uri = new Uri("http://localhost:9092");
+            var options = new KafkaOptions(uri);
+            var router = new BrokerRouter(options);
+            var producer = new Producer(router);
+            producer.SendMessageAsync(topic, messages).Wait();
+
+        }
+    }
+}
